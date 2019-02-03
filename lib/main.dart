d030941b88d2d1331a7a0302de3d6b468a25473b
Marcio Teixeira Quimbundo <mquimbundo@ucall.co.ao>
2019-02-03 09:36:23 +0100

    config icon, text center and textfield added

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,15 @@ void main() => runApp(MaterialApp(
       title: "Kiviagem Mock Up",
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
+      theme: appTheme,
     ));
 
 Color firstColor = Color(0xFFF47D15);
 Color secondColor = Color(0xFFEF7772C);
+ThemeData appTheme =
+    ThemeData(primaryColor: Color(0xFFF3791A), fontFamily: 'Oxygen');
+
+List<String> locations = ['Boston (BOS)', 'New York City (JFK)'];
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -23,6 +28,11 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
+const TextStyle dropDownLabelStyle =
+    TextStyle(color: Colors.white, fontSize: 16.0);
+const TextStyle dropDownMenuItemStyle =
+    TextStyle(color: Colors.black, fontSize: 16.0);
+
 class HomeScreenTopPart extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -31,6 +41,9 @@ class HomeScreenTopPart extends StatefulWidget {
 }
 
 class _HomeScreenTopState extends State<HomeScreenTopPart> {
+
+  var selectedLocationIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -38,10 +51,69 @@ class _HomeScreenTopState extends State<HomeScreenTopPart> {
         ClipPath(
           clipper: CustomShapeClipper(),
           child: Container(
-              height: 400.0,
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [firstColor, secondColor])),
+            height: 400.0,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [firstColor, secondColor])),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 50.0),
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.location_on, color: Colors.white),
+                      SizedBox(width: 16.0),
+                      PopupMenuButton(
+                          onSelected: (index) {
+                            setState(() {
+                              selectedLocationIndex = index;
+                            });
+                          },
+                          child: Row(
+                            children: <Widget>[
+                              Text(locations[selectedLocationIndex],
+                                  style: dropDownLabelStyle),
+                              Icon(
+                                Icons.keyboard_arrow_down,
+                                color: Colors.white,
+                              )
+                            ],
+                          ),
+                          itemBuilder: (BuildContext context) =>
+                              <PopupMenuItem<int>>[
+                                PopupMenuItem(
+                                  child: Text(locations[0],
+                                      style: dropDownMenuItemStyle),
+                                  value: 0,
+                                ),
+                                PopupMenuItem(
+                                  child: Text(locations[1],
+                                      style: dropDownMenuItemStyle),
+                                  value: 1,
+                                ),
+                              ]),
+                              Spacer(),
+                              Icon(Icons.settings, color: Colors.white,)
+                    ],
                   ),
+                ),
+                SizedBox(height: 50.0),
+                Text('Onde você\nquer ir??', style: TextStyle(fontSize: 26.0, color: Colors.white), textAlign: TextAlign.center),
+                SizedBox(height: 30.0),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 32.0),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                    child: TextField(
+                      controller: TextEditingController(text: locations[0]),
+                      cursorColor: appTheme.primaryColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         )
       ],
     );
