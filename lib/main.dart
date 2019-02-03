12324ac2f6512f1e2160cd0cd45bd434eb13f8c5
Marcio Teixeira Quimbundo <mquimbundo@ucall.co.ao>
2019-02-03 09:49:24 +0100

    TextField decoration finished

--- lib/main.dart
+++ lib/main.dart
@@ -106,10 +106,23 @@ class _HomeScreenTopState extends State<HomeScreenTopPart> {
                     elevation: 5.0,
                     borderRadius: BorderRadius.all(Radius.circular(30.0)),
                     child: TextField(
-                      controller: TextEditingController(text: locations[0]),
+                      controller: TextEditingController(text: locations[1]),
+                      style: dropDownMenuItemStyle,
                       cursorColor: appTheme.primaryColor,
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 14.0),
+                        suffixIcon: Material(
+                          elevation: 2.0,
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                          child: Icon(Icons.search, color: Colors.black,),
+                        ),
+                        border: InputBorder.none,
+                      ),
                     ),
                   ),
+                ),
+                Row(
+
                 ),
               ],
             ),
