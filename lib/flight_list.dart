78b03a66aa1fddc5820fb571628e3aa0557d9d6b
Marcio Teixeira Quimbundo <mquimbundo@ucall.co.ao>
2019-02-03 23:52:42 +0100

    card adjustment finished

--- lib/flight_list.dart
+++ lib/flight_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:kiviagem/CustomShapeClipper.dart';
+import 'package:kiviagem/main.dart';
 
 final Color discountBackgroundColor = Color(0xFFFFE08D);
 final Color flightBorderColor = Color(0xFFE6E6E6);
@@ -11,12 +13,85 @@ class FlightListingScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Resultado da pesquisa"),
         centerTitle: true,
+        elevation: 0.0,
         leading: InkWell(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Icon(Icons.arrow_back)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(Icons.arrow_back)),
       ),
+      body: Column(
+        children: <Widget>[
+          FlightListTopPart(),
+        ],
+      ),
+    );
+  }
+}
+
+class FlightListTopPart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        ClipPath(
+          clipper: CustomShapeClipper(),
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [firstColor, secondColor])),
+            height: 160.0,
+          ),
+        ),
+        Column(
+          children: <Widget>[
+            SizedBox(height: 20.0),
+            Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(16.0))),
+              margin: EdgeInsets.symmetric(horizontal: 16.0),
+              elevation: 10.0,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 5,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Boston (BOS)",
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                          Divider(
+                            color: Colors.grey,
+                          ),
+                          Text(
+                            "New York City (JFK)",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16.0),
+                          )
+                        ],
+                      ),
+                    ),
+                    Spacer(),
+                    Expanded(
+                      flex: 1,
+                                          child: Icon(
+                        Icons.import_export,
+                        color: Colors.black,
+                        size: 32.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
